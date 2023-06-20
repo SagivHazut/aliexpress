7b44479f738e8ae6928287d53b95b5fd889988fa
sagivhazut <sagivhazut@gmail.com>
2023-06-20 23:50:12 +0300

    videos

--- src/components/VideoScroll.jsx
+++ src/components/VideoScroll.jsx
@@ -2,9 +2,10 @@ import React, { useState, useEffect, useRef } from 'react'
 import axios from 'axios'
 import LoadingSpinner from './LoadingSpinner'
 
-const VideoScroll = () => {
+const YourComponent = () => {
   const [videos, setVideos] = useState([])
-  const [currentVideoIndex, setCurrentVideoIndex] = useState(0)
+  const [visibleVideos, setVisibleVideos] = useState([])
+  const [isLoading, setIsLoading] = useState(true)
   const containerRef = useRef(null)
 
   useEffect(() => {
@@ -15,7 +16,8 @@ const VideoScroll = () => {
           {
             params: {
               language: 'en',
-              category_ids: '320,3,100001205',
+              category_ids:
+                '200133142,200000854,200003494,200000345,201336907,201169002',
               page_size: 50,
               page_no: 1,
               max_sale_price: '3000',
@@ -32,8 +34,10 @@ const VideoScroll = () => {
         }
 
         setVideos((prevVideos) => [...prevVideos, ...newData])
+        setIsLoading(false)
       } catch (error) {
         console.error(error)
+        setIsLoading(false)
       }
     }
 
@@ -41,18 +45,15 @@ const VideoScroll = () => {
   }, [])
 
   useEffect(() => {
-    const handleScroll = (entries) => {
+    const handleIntersection = (entries) => {
       entries.forEach((entry) => {
+        const videoElement = entry.target
         if (entry.isIntersecting) {
-          const videoElement = entry.target
-          videoElement.play()
-          setCurrentVideoIndex(
-            parseInt(videoElement.getAttribute('data-index'))
-          )
+          videoElement.play().catch((error) => {
+            console.error('Failed to play video:', error)
+          })
         } else {
-          const videoElement = entry.target
           videoElement.pause()
-          videoElement.currentTime = 0
         }
       })
     }
@@ -60,60 +61,139 @@ const VideoScroll = () => {
     const options = {
       root: null,
       rootMargin: '0px',
-      threshold: 0.5, // Adjust this threshold as needed
+      threshold: 0.5,
     }
 
-    const observer = new IntersectionObserver(handleScroll, options)
+    const observer = new IntersectionObserver(handleIntersection, options)
 
-    const videosElements = document.querySelectorAll('.video-element')
-    videosElements.forEach((videoElement) => {
-      observer.observe(videoElement)
-    })
+    if (visibleVideos.length > 0) {
+      visibleVideos.forEach((video) => {
+        const videoElement = document.getElementById(`video-${video.id}`)
+        if (videoElement) {
+          observer.observe(videoElement)
+        }
+      })
+    }
 
     return () => {
-      observer.disconnect()
+      if (visibleVideos.length > 0) {
+        visibleVideos.forEach((video) => {
+          const videoElement = document.getElementById(`video-${video.id}`)
+          if (videoElement) {
+            observer.unobserve(videoElement)
+          }
+        })
+      }
     }
-  }, [])
+  }, [visibleVideos])
 
-  const filteredVideos = videos.filter((video) => {
-    const url = video.product_video_url
-    return url
-  })
+  useEffect(() => {
+    const filteredVideos = videos.filter((video) => video.product_video_url)
+    setVisibleVideos(filteredVideos)
+  }, [videos])
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const container = containerRef.current
+      if (!container) return
+
+      const { scrollTop, clientHeight } = container
+      const threshold = 0.8
+
+      visibleVideos.forEach((video) => {
+        const videoElement = document.getElementById(`video-${video.id}`)
+        if (videoElement) {
+          const { top, height } = videoElement.getBoundingClientRect()
+          const isVisible =
+            top + height >= 0 && top - height <= clientHeight * threshold
+          if (isVisible && videoElement.paused) {
+            videoElement.play().catch((error) => {
+              console.error('Failed to play video:', error)
+            })
+          } else if (!isVisible && !videoElement.paused) {
+            videoElement.pause()
+          }
+        }
+      })
+    }
+
+    const container = containerRef.current
+    if (container) {
+      container.addEventListener('scroll', handleScroll)
+    }
+
+    return () => {
+      if (container) {
+        container.removeEventListener('scroll', handleScroll)
+      }
+    }
+  }, [visibleVideos])
 
   return (
-    <div>
-      {filteredVideos.length > 0 ? (
-        <div ref={containerRef} style={{ height: '100vh', overflow: 'hidden' }}>
-          {filteredVideos.map((video, index) => (
-            <a
-              href={video.promotion_link}
-              target="_blank"
-              rel="noopener noreferrer"
+    <>
+      {isLoading ? (
+        <LoadingSpinner />
+      ) : (
+        <div ref={containerRef} style={{ height: '800px', overflow: 'scroll' }}>
+          {visibleVideos.map((video, index) => (
+            <div
+              className="video-container"
+              style={{ display: 'block', background: 'black' }}
+              key={index}
             >
-              <video
-                id={`video-${index}`}
-                key={video.id}
-                src={video.product_video_url}
-                autoPlay
-                loop
-                style={{
-                  width: '100%',
-                  objectFit: 'cover',
-                  marginTop: -30,
-                  marginBottom: 20,
-                }}
-                className="video-element"
-                data-index={index}
-                muted
+              <div className="video-wrap">
+                <video
+                  width="560"
+                  height="315"
+                  poster={video.product_image_url}
+                  className="disable-download"
+                  controlsList="nodownload"
+                  id={`video-${video.id}`}
+                  src={video.product_video_url.replace(
+                    'http://cloud.video.taobao.com',
+                    'https://video.aliexpress-media.com'
+                  )}
+                  style={{
+                    backgroundColor: 'transparent',
+                    display: 'block',
+                    height: 700,
+                  }}
+                  autoPlay={false}
+                  controls={false}
+                  onClick={(e) =>
+                    e.target.paused ? e.target.play() : e.target.pause()
+                  }
+                ></video>
+              </div>
+              <img
+                alt={video.product_title}
+                id="poster"
+                className="poster"
+                src={video.product_image_url}
+                style={{ display: 'none' }}
+                data-spm-anchor-id="a2g0o.detail.1000017.i0.73684deeF61nkB"
               />
-            </a>
+              <a
+                href={video.promotion_link}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <span
+                  className="play-icon"
+                  data-spm-anchor-id="a2g0o.detail.1000017.i1.73684deeF61nkB"
+                  style={{ background: 'none', color: 'white' }}
+                >
+                  {video.product_title}
+                  <br />
+                  Click here
+                </span>
+              </a>
+            </div>
           ))}
         </div>
-      ) : (
-        <LoadingSpinner />
       )}
-    </div>
+    </>
   )
 }
 
-export default VideoScroll
+export default YourComponent
