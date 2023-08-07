495796f38bbb7f9152ca180e8ca9fa5b32b0a65b
sagivhazut <sagivhazut@gmail.com>
2023-08-07 05:23:24 +0300

    affiliate link component

--- src/pages/AffiliateLink.jsx
+++ src/pages/AffiliateLink.jsx
@@ -1,34 +1,40 @@
-import React, { useState, useEffect } from 'react'
-import { useParams } from 'react-router-dom'
+import React, { useState } from 'react'
 import axios from 'axios'
-import { useSelector } from 'react-redux'
-import ErrorPopup from '../components/ErrorPopup'
-import _ from 'lodash'
-import Item from '../components/Item'
 
-export const AffiliateLink = ({ setSearchRes, searchRes }) => {
+export const AffiliateLink = () => {
   const [productUrl, setProductUrl] = useState('')
-  const [discountInfo, setDiscountInfo] = useState(null)
-  const [items, setItems] = useState(null)
-  const [searchQuery, setSearchQuery] = useState('')
-  const [parsedData, setParsedData] = useState([])
-  const [isLoading, setIsLoading] = useState(true)
-  const [isLoadingMore, setIsLoadingMore] = useState(false)
-  const [showFilter, setShowFilter] = useState(false)
+  const [error, setError] = useState(null)
 
-  const checkDiscount = () => {
-    fetch(`YOUR_API_ENDPOINT?url=${encodeURIComponent(productUrl)}`)
-      .then((response) => response.json())
-      .then((data) => {
-        setDiscountInfo(data)
-      })
-      .catch((error) => {
-        console.error('Error checking discount:', error)
-      })
+  const [parsedData, setParsedData] = useState([])
+
+  async function fetchData() {
+    const storedCountry = localStorage.getItem('country')
+    try {
+      const response = await axios.get(
+        'https://3nbe6jbrg0.execute-api.us-east-1.amazonaws.com/promo-link-shop',
+        {
+          params: {
+            link: productUrl,
+          },
+          mode: 'no-cors',
+        }
+      )
+
+      const newData = response.data
+      if (response.status !== 200) {
+        setParsedData('No discount available')
+      } else {
+        setParsedData(newData)
+      }
+    } catch (error) {
+      setError(
+        storedCountry === 'IL'
+          ? 'לפעמים צריך רק רענון קטן בשביל שזה יעבוד '
+          : " Often, a slight refresh is all that's needed to optimize its performance"
+      )
+    }
   }
-  const handleInputChange = (event) => {
-    setSearchQuery(event.target.value)
-  }
+
   return (
     <div className="container mx-auto py-20">
       <h1 className="text-4xl font-bold mb-5">Check for Discount</h1>
@@ -74,29 +80,24 @@ export const AffiliateLink = ({ setSearchRes, searchRes }) => {
         className="w-full px-4 py-3 rounded-lg border-2 border-gray-300 focus:outline-none focus:border-blue-500"
       />
       <button
-        onClick={checkDiscount}
+        onClick={fetchData}
         className="px-6 py-3 mt-4 bg-blue-500 text-white rounded-lg hover:bg-blue-600 focus:outline-none"
       >
         Check
       </button>
-      {discountInfo && (
-        <div className="mt-5">
-          <p>Discount: {discountInfo.discount * 100}%</p>
-          <p>Discounted Price: {discountInfo.discountedPrice} USD</p>
+
+      {parsedData && (
+        <div className="mt-4">
+          <p className="text-blue-500 font-bold">Discount Link:</p>
+          <a
+            href={parsedData}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-blue-500 hover:underline"
+          >
+            {parsedData}
+          </a>
         </div>
-      )}
-      {items && (
-        <Item
-          isLoading={isLoading}
-          post={parsedData}
-          setShowFilter={setShowFilter}
-          showFilter={showFilter}
-          isLoadingMore={isLoadingMore}
-          handleInputChange={handleInputChange}
-          searchQuery={searchQuery}
-          searchRes={searchRes}
-          setSearchRes={setSearchRes}
-        />
       )}
     </div>
   )
